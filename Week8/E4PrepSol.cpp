73c8eee32131f657b6dc6c43e4821b3ef21725c9
Jalynn Nicoly <Jalynnn@colostate.edu>
2026-03-05 21:41:55 -0700

    FridayPrep

---
+++ Week8/E4PrepSol.cpp
@@ -0,0 +1,46 @@
+/*
+Write a function named matrixProduct that calculates the product 
+of all elements in a 2D array of integers.
+
+Requirements
+* Function name: matrixProduct
+* Parameter 1: A 2D integer array where the second dimension is 
+always of size 3
+* Parameter 2: An integer representing the size of the first 
+dimension (number of rows)
+* Returns: The product of all numbers in the 2D array
+
+Example 1:
+    int arrayName[1][3] = {{2, 3, 5}};
+    matrixProduct(arrayName, 1); // Returns 30 (2 * 3 * 5)
+
+Example 2:
+    int arrayName2[2][3] = {{1, 2, 1}, {4, 5, 2}};
+    matrixProduct(arrayName2, 2); // Returns 80 (1 * 2 * 1 * 4 * 5 * 2)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int matrixProduct(int arrayName[][3], int rows) {
+    int sum = 1;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < 3; j++) {
+            sum *= arrayName[i][j];
+        }
+    }
+    return sum;
+}
+
+int main() {
+// Example 1:
+    int arrayName[1][3] = {{2, 3, 5}};
+    int sum = matrixProduct(arrayName, 1); // Returns 30 (2 * 3 * 5)
+    cout << "The sum for the first array is: " << sum << endl;
+
+// Example 2:
+    int arrayName2[2][3] = {{1, 2, 1}, {4, 5, 2}};
+    int sum2 = matrixProduct(arrayName2, 2); // Returns 80 (1 * 2 * 1 * 4 * 5 * 2)
+    cout << "The sum for the second array is: " << sum2 << endl;
+}
